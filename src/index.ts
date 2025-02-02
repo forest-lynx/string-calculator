5bfefef283d5b25901a92ed0cd7f7eba2e995b4e
trot505 <trot505@yandex.ru>
2025-02-02 19:09:45 +0400

    doc

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,2 @@
-export { StringCalculator, calculate } from './StringCalculator';
+export { StringCalculator, stringCalculate } from './StringCalculator';
 export type { StringCalculatorOptions, IStringCalculator } from './types';
